8ff94dd455a175e585ca968df3300bf6f9a6a0d0
vickonrails <victorofoegbu0009@gmail.com>
2023-04-15 10:20:03 +0100

    chore: modify config settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,21 @@
 import { type Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['DM Sans', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        'crispy': '16px 16px 70px rgba(81, 67, 188, 0.1), -8px -8px 70px rgba(81, 67, 188, 0.1);',
+        'light': '0px 1px 2px rgba(16, 24, 40, 0.05)'
+      },
+      colors: {
+        'primary': '#683DF5'
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
