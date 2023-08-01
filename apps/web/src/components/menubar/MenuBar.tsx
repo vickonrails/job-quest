e7fc030bdfe92cba5b380a867b104821b287caf0
vickonrails <victorofoegbu0009@gmail.com>
2023-08-01 19:17:23 +0100

    refactor: add menubar storybook, further refactors

--- apps/web/src/components/menubar/MenuBar.tsx
+++ apps/web/src/components/menubar/MenuBar.tsx
@@ -1,38 +1,48 @@
 import * as RadixMenuBar from '@radix-ui/react-menubar'
+import clsx from 'clsx';
 import { type FC } from 'react';
 
 interface MenuBarProp extends RadixMenuBar.MenubarProps {
     trigger: React.ReactNode
+    triggerProps?: RadixMenuBar.MenubarTriggerProps
 }
 
 // TODO: fix x index
-const MenuBar: FC<MenuBarProp> = ({ trigger, children, ...rest }) => (
-    <RadixMenuBar.Root className="text-base-col" {...rest}>
-        <RadixMenuBar.Menu>
-            <RadixMenuBar.Trigger>
-                {trigger}
-            </RadixMenuBar.Trigger>
-            <RadixMenuBar.Portal>
-                <RadixMenuBar.Content className="bg-white rounded-md p-2 min-w-150 shadow-sm border-solid border border-gray-100">
-                    {children}
-                </RadixMenuBar.Content>
-            </RadixMenuBar.Portal>
-        </RadixMenuBar.Menu>
-    </RadixMenuBar.Root>
-);
+const MenuBar: FC<MenuBarProp> = ({ trigger, triggerProps, children, ...rest }) => {
+    const { className } = triggerProps ?? {}
+    return (
+        <RadixMenuBar.Root className="text-base-col" {...rest}>
+            <RadixMenuBar.Menu>
+                <RadixMenuBar.Trigger
+                    className={clsx(className)}
+                    {...triggerProps}
+                >
+                    {trigger}
+                </RadixMenuBar.Trigger>
+                <RadixMenuBar.Portal>
+                    <RadixMenuBar.Content className="bg-white rounded-md p-2 min-w-150 shadow-sm border-solid border border-gray-100 mt-1 text-light-text">
+                        {children}
+                    </RadixMenuBar.Content>
+                </RadixMenuBar.Portal>
+            </RadixMenuBar.Menu>
+        </RadixMenuBar.Root>
+    );
 
+}
 type MenuItemProps = RadixMenuBar.MenuItemProps & {
     icon?: React.ReactNode
 }
 
-const MenuItem: FC<MenuItemProps> = ({ icon, children, ...rest }) => {
+const MenuItem: FC<MenuItemProps> = ({ icon, className, children, ...rest }) => {
+    // TODO: add variant prop for menu Item
     return (
         <RadixMenuBar.Item
-            className="text-sm px-2 py-1 flex items-center rounded-md hover:bg-purple-50 hover:cursor-pointer " {...rest}
+            className={clsx('transition text-sm px-2 py-1 flex items-center rounded-md hover:bg-gray-100 hover:cursor-pointer', className)}
+            {...rest}
         >
-            <span className="mr-2">{icon}</span>
+            <span className="pr-2">{icon}</span>
             <span>{children}</span>
-        </RadixMenuBar.Item>
+        </RadixMenuBar.Item >
     )
 }
 const Separator = () => (
