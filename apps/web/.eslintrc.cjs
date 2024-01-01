0a808854444c9374a58ca0f53f3cfcf2cbf88df9
vickonrails <victorofoegbu0009@gmail.com>
2024-01-01 12:26:57 +0100

    chore: remove storybook dependencies

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -15,7 +15,7 @@ const config = {
     project: path.join(__dirname, "tsconfig.json")
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": ["warn", {
       prefer: "type-imports",
