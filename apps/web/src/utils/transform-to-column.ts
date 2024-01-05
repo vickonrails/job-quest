97a1d8d228a7b352471459494d73e10d19ee246c
vickonrails <victorofoegbu0009@gmail.com>
2024-01-05 17:43:35 +0100

    fix: refactor types to transform jobs to columns

--- apps/web/src/utils/transform-to-column.ts
+++ apps/web/src/utils/transform-to-column.ts
@@ -2,74 +2,6 @@
 import { type Job } from '@lib/types';
 import { v4 as uuid } from 'uuid';
 
-export function sortByOrder(a: Job, b: Job) {
-    if (a.order_column === undefined || a.order_column === null) return 1;
-    if (b.order_column === undefined || b.order_column === null) return -1;
-
-    const orderA = Number(a.order_column);
-    const orderB = Number(b.order_column);
-
-    return orderA - orderB;
-}
-
-
-export function transformJobs(jobs: Job[]) {
-    const kanbanColumns: KanbanColumn[] = [
-        {
-            id: uuid(),
-            title: 'Bookmarked',
-            columnStatus: ApplicationStatus.Bookmarked,
-            jobs: [],
-        },
-        {
-            id: uuid(),
-            title: 'Applying',
-            columnStatus: ApplicationStatus.Applying,
-            jobs: []
-        },
-        {
-            id: uuid(),
-            title: 'Applied',
-            columnStatus: ApplicationStatus.Applied,
-            jobs: []
-        },
-        {
-            id: uuid(),
-            title: 'Interviewing',
-            columnStatus: ApplicationStatus.Interviewing,
-            jobs: []
-        },
-        {
-            id: uuid(),
-            title: 'Negotiating',
-            columnStatus: ApplicationStatus.Negotiating,
-            jobs: []
-        },
-        // {
-        //     id: uuid(),
-        //     title: 'Hired',
-        //     columnStatus: ApplicationStatus.Interviewing,
-        //     jobs: []
-        // },
-        // {
-        //     id: uuid(),
-        //     title: 'Rejected',
-        //     columnStatus: ApplicationStatus.Rejected,
-        //     jobs: []
-        // }
-    ] as const
-
-    // TODO: fix the typings here
-
-    jobs.sort(sortByOrder).forEach(job => {
-        const colIndex = job.status || 0;
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        kanbanColumns[colIndex].jobs.push(job)
-    });
-
-    return kanbanColumns
-}
-
 export enum ApplicationStatus {
     Bookmarked,
     Applying,
@@ -80,10 +12,74 @@ export enum ApplicationStatus {
     Rejected,
 }
 
-
 export interface KanbanColumn {
     id: string,
     title: string,
     columnStatus: ApplicationStatus,
     jobs: Job[]
+}
+
+export function sortByOrder(a: Job, b: Job) {
+    if (a.order_column === undefined || a.order_column === null) return 1;
+    if (b.order_column === undefined || b.order_column === null) return -1;
+
+    const orderA = Number(a.order_column);
+    const orderB = Number(b.order_column);
+
+    return orderA - orderB;
+}
+
+const kanbanColumns: KanbanColumn[] = [
+    {
+        id: uuid(),
+        title: 'Bookmarked',
+        columnStatus: ApplicationStatus.Bookmarked,
+        jobs: [],
+    },
+    {
+        id: uuid(),
+        title: 'Applying',
+        columnStatus: ApplicationStatus.Applying,
+        jobs: []
+    },
+    {
+        id: uuid(),
+        title: 'Applied',
+        columnStatus: ApplicationStatus.Applied,
+        jobs: []
+    },
+    {
+        id: uuid(),
+        title: 'Interviewing',
+        columnStatus: ApplicationStatus.Interviewing,
+        jobs: []
+    },
+    {
+        id: uuid(),
+        title: 'Negotiating',
+        columnStatus: ApplicationStatus.Negotiating,
+        jobs: []
+    },
+    // {
+    //     id: uuid(),
+    //     title: 'Hired',
+    //     columnStatus: ApplicationStatus.Interviewing,
+    //     jobs: []
+    // },
+    // {
+    //     id: uuid(),
+    //     title: 'Rejected',
+    //     columnStatus: ApplicationStatus.Rejected,
+    //     jobs: []
+    // }
+] as const
+
+export function transformJobs(jobs: Job[]) {
+    jobs.sort(sortByOrder).forEach(job => {
+        const colIndex = job.status || 0;
+        if (!kanbanColumns) return;
+        kanbanColumns?.[colIndex]?.jobs.push(job)
+    });
+
+    return kanbanColumns
 }
