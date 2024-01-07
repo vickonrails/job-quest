e1568acf4102ba893c0990700e077b34b6bc5a0f
vickonrails <victorofoegbu0009@gmail.com>
2024-01-07 12:53:54 +0100

    fix: breaking build in getMovingItemData

--- apps/web/src/components/kanban/core/getMovingItemData.ts
+++ apps/web/src/components/kanban/core/getMovingItemData.ts
@@ -14,23 +14,30 @@ export function getMovingItemData(result: DropResult, involvedColumn: InvolvedCo
     const { start, finish } = involvedColumn;
     const sameColumn = start.id === finish.id;
 
-    const sourceJobs = Array.from(involvedColumn.start.jobs);
+    const sourceJobs = Array.from(involvedColumn.start.jobs) || [];
     const destinationJobs = Array.from(involvedColumn.finish.jobs);
 
     const movingItem = sameColumn ? sourceJobs[source.index] : sourceJobs.splice(source.index, 1)[0];
 
     if (!movingItem) return;
+    if (sourceJobs.length === 0 && destinationJobs.length === 0) return;
 
     if (sameColumn) {
         const downwards = destination.index < source.index;
-        if (destination.index === 0) {
-            movingItemOrder = sourceJobs[0].order_column - ORDER_DISTANCE;
-        } else if (destination.index === sourceJobs.length - 1) {
-            movingItemOrder = sourceJobs[sourceJobs.length - 1].order_column + ORDER_DISTANCE;
+        if (destination.index === 0 && sourceJobs.length > 0) {
+            const firstItemOrder = sourceJobs[0] && sourceJobs[0].order_column;
+            if (!firstItemOrder) return;
+            movingItemOrder = firstItemOrder - ORDER_DISTANCE;
+        } else if ((destination.index === sourceJobs.length - 1) && sourceJobs.length > 0) {
+            const lastIndex = sourceJobs.length - 1;
+            const lastItemOrder = sourceJobs[lastIndex] && sourceJobs[lastIndex]?.order_column
+            if (!lastItemOrder) return;
+            movingItemOrder = lastItemOrder + ORDER_DISTANCE;
         } else {
             const beforeItem = sourceJobs[downwards ? destination.index - 1 : destination.index];
             const afterItem = sourceJobs[downwards ? destination.index : destination.index + 1];
             if (!beforeItem?.order_column || afterItem?.order_column === null) return;
+            if (typeof afterItem?.order_column !== 'number') return;
             movingItemOrder = (afterItem?.order_column + beforeItem?.order_column) / 2;
         }
 
@@ -39,7 +46,10 @@ export function getMovingItemData(result: DropResult, involvedColumn: InvolvedCo
             const newItemOrder = destinationJobs[0]?.order_column || 0;
             movingItemOrder = newItemOrder - ORDER_DISTANCE;
         } else if (destination.index === destinationJobs.length) {
-            movingItemOrder = destinationJobs[destinationJobs.length - 1].order_column + ORDER_DISTANCE;
+            const lastIndex = destinationJobs.length - 1
+            const lastItemOrder = destinationJobs[lastIndex]?.order_column;
+            if (!lastItemOrder) return;
+            movingItemOrder = lastItemOrder + ORDER_DISTANCE;
         } else {
             const beforeItem = destinationJobs[destination.index - 1];
             const afterItem = destinationJobs[destination.index];
@@ -48,7 +58,6 @@ export function getMovingItemData(result: DropResult, involvedColumn: InvolvedCo
         }
 
         movingItem.status = finish.columnStatus;
-
         destinationJobs.push(movingItem);
     }
 
