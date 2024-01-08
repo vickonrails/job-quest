3563e7499fa5f30b17ca75ae2da8a7200ca86c47
vickonrails <victorofoegbu0009@gmail.com>
2024-01-08 10:17:34 +0100

    fix: improve authentication page

--- packages/ui/src/components/auth/AuthCard.tsx
+++ packages/ui/src/components/auth/AuthCard.tsx
@@ -1,7 +1,7 @@
 
+import { type FC, type HTMLAttributes } from 'react'
 import { cn } from '../../utils'
 import { Logo } from '../logo'
-import { type FC, type HTMLAttributes } from 'react'
 
 type AuthCardProps = HTMLAttributes<HTMLElement>
 
@@ -14,7 +14,7 @@ export const AuthCard: FC<AuthCardProps> = ({ children, className, ...rest }) =>
         <main className={cn("flex min-h-screen items-start flex-col", className)}>
             <Logo className="p-5" />
             <div
-                className="rounded-lg p-4 w-full m-4 mt-20 py-10 max-w-md md:max-w-lg md:flex md:mx-auto md:shadow-crispy xl:max-w-xl xl:m-auto relative border"
+                className="rounded-lg p-4 w-full m-4 mt-20 py-10 md:flex md:mx-auto md:shadow-crispy xl:max-w-xl xl:m-auto relative border"
                 {...rest}
             >
                 {children}
