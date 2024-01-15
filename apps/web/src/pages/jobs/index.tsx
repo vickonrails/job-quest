48d865c86d9990b02e897d08fc2260fb676d24a1
vickonrails <victorofoegbu0009@gmail.com>
2024-01-15 22:08:37 +0100

    chore: cleanup table component

--- apps/web/src/pages/jobs/index.tsx
+++ apps/web/src/pages/jobs/index.tsx
@@ -1,72 +1,34 @@
 import JobsKanban from '@components/kanban/kanban-container';
 import { Layout } from '@components/layout';
 import { Spinner } from '@components/spinner';
-import JobsTable from '@components/table/job/JobsTable';
 import { useJobs } from '@hooks';
 import { createPagesServerClient, type Session } from '@supabase/auth-helpers-nextjs';
-import { cn } from '@utils/cn';
 import { type Database } from 'lib/database.types';
 import { type Job, type Profile } from 'lib/types';
-import { AlignStartHorizontal, TableIcon } from 'lucide-react';
 import { type GetServerSideProps } from 'next';
-import { useState, type HTMLAttributes } from 'react';
-
-type View = 'kanban' | 'table';
+import { useState } from 'react';
 
 const Tracker = ({ session, profile, jobs }: {
     session: Session, profile: Profile, jobs: Job[]
 }) => {
     const { data } = useJobs({ initialData: jobs });
     const [isUpdating, setIsUpdating] = useState(false)
-    const [view, setView] = useState<View>('kanban');
-    const isTable = view === 'table';
 
     return (
         <Layout session={session} profile={profile}>
-            <section className="flex justify-between items-center">
-                <h1 className="text-2xl flex font-bold gap-2 items-center">
-                    <span>
-                        Jobs
-                    </span>
+            <section className="flex justify-between items-center mb-3">
+                <h1 className="text-xl flex font-bold gap-2 items-center">
+                    <span>Jobs</span>
                     {isUpdating && <Spinner />}
                 </h1>
-                <ViewSwitcher view={view} setView={setView} />
             </section>
-            {isTable ? <JobsTable jobs={data?.jobs ?? []} /> :
-                <JobsKanban
-                    jobs={data?.jobs ?? []}
-                    onUpdateStart={() => setIsUpdating(true)}
-                    onUpdateEnd={() => setIsUpdating(false)}
-                />
-            }
+
+            <JobsKanban
+                jobs={data?.jobs ?? []}
+                onUpdateStart={() => setIsUpdating(true)}
+                onUpdateEnd={() => setIsUpdating(false)}
+            />
         </Layout>
-    )
-}
-
-function ViewSwitcher({ view, setView }: { view: View, setView: (view: View) => void }) {
-    return (
-        <section className="flex justify-end mb-4">
-            <article className="bg-gray-300 rounded-md p-1">
-                <IconButton
-                    active={view === 'table'}
-                    onClick={() => setView('table')}
-                >
-                    <TableIcon size={18} />
-                </IconButton>
-                <IconButton
-                    active={view === 'kanban'}
-                    onClick={() => setView('kanban')}
-                >
-                    <AlignStartHorizontal size={18} />
-                </IconButton>
-            </article>
-        </section>
-    )
-}
-
-function IconButton({ active, className, ...props }: HTMLAttributes<HTMLButtonElement> & { active: boolean }) {
-    return (
-        <button className={cn('p-2 rounded-md text-gray-600', active && 'bg-white', className)} {...props}></button>
     )
 }
 
