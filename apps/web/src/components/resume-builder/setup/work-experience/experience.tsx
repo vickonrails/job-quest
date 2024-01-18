b8c3ea30b0ba14510962d9e3d47efb73afe9d950
vickonrails <victorofoegbu0009@gmail.com>
2024-01-18 13:20:27 +0100

    chore: temporarily disable highlights

--- apps/web/src/components/resume-builder/setup/work-experience/experience.tsx
+++ apps/web/src/components/resume-builder/setup/work-experience/experience.tsx
@@ -1,65 +1,103 @@
-import { type WorkExperience } from '@lib/types'
+import { AlertDialog } from '@components/alert-dialog'
+import { type Database } from '@lib/database.types'
+import { type WorkExperienceInsertDTO } from '@lib/types'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Trash2 } from 'lucide-react'
-import { useRef } from 'react'
+import { useState } from 'react'
 import { useFieldArray, useFormContext } from 'react-hook-form'
+import { useDeleteModal } from 'src/hooks/useDeleteModal'
 import { useSetupContext } from 'src/hooks/useSetupContext'
-import { type FormFields } from 'src/pages/profile/setup'
+import { defaultWorkExperience, type FormFields } from 'src/pages/profile/setup'
 import { Button, Input } from 'ui'
 import { StepContainer } from '../container'
-import { type RefTypeWorkExperience } from './highlights'
+// import { Highlights, type RefTypeWorkExperience } from './highlights'
 
 export function WorkExperience() {
+    const client = useSupabaseClient<Database>()
     const { register, control, handleSubmit, formState: { isSubmitting } } = useFormContext<FormFields>()
     const { fields, append, remove } = useFieldArray({
         name: 'workExperience',
         control,
         keyName: '_id'
     })
-    const { next, updateWorkExperience, deleteExperience, session } = useSetupContext()
-    const highlightRef = useRef<RefTypeWorkExperience>(null)
+
+    const [removeIdx, setRemoveIdx] = useState<number>();
+
+    const { showDeleteDialog, onCancel, handleDelete, loading, setIsOpen, isOpen } = useDeleteModal({
+        onDelete: async (id: string) => {
+            const { error } = await client.from('work_experience').delete().eq('id', id)
+            if (error) throw error;
+        }
+    })
+
+    const { next, updateWorkExperience, session } = useSetupContext()
+    // const highlightRef = useRef<RefTypeWorkExperience>(null)
     const onSubmit = async (values: FormFields) => {
         if (!session?.user.id) return;
-        await updateWorkExperience(values, session?.user.id)
+        await updateWorkExperience.mutateAsync({ values, userId: session?.user.id })
         next();
     }
 
     // TODO: add a confirmation dialog before deleting the work experience
     // TODO: optimistic update and rollback if the request fails
-    const deleteWorkExperience = async (experience: WorkExperience, index: number) => {
-        if (!session?.user.id) return;
-        await deleteExperience(experience.id)
-        remove(index);
+    const onDeleteClick = (experience: WorkExperienceInsertDTO, index: number) => {
+        if (!experience.created_at) {
+            remove(index);
+            return;
+        }
+
+        setRemoveIdx(index);
+
+        if (!experience.id) return
+        showDeleteDialog({ ...experience, id: experience.id });
+    }
+
+    const onDeleteOk = async () => {
+        await handleDelete();
+        remove(removeIdx)
     }
 
     return (
-        <StepContainer title="Work Experience">
-            <p className="mb-4 text-gray-500">Work Experience.</p>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {fields.map((field, index) => (
-                    <section className="p-4 border mb-8" key={field.id}>
-                        <section className="mb-4 grid grid-cols-2 gap-3 rounded-md" key={field._id}>
-                            <Input label="Company Name" placeholder="Company name" {...register(`workExperience.${index}.company_name`)} />
-                            <Input label="Title" placeholder="Job titled held..." {...register(`workExperience.${index}.job_title`)} />
-                            <Input label="Location" placeholder="Location..." {...register(`workExperience.${index}.location`)} />
-                            <Input type="date" label="Start Date" placeholder="Start Date..." {...register(`workExperience.${index}.start_date`)} />
-                            <Input type="date" label="End Date" placeholder="End Date..." {...register(`workExperience.${index}.end_date`)} />
+        <>
+            <StepContainer title="Work Experience">
+                <p className="mb-4 text-gray-500">Work Experience.</p>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {fields.map((field, index) => (
+                        <section className="p-4 border mb-8" key={field.id}>
+                            <section className="mb-4 grid grid-cols-2 gap-3 rounded-md" key={field._id}>
+                                <Input label="Company Name" placeholder="Company name" {...register(`workExperience.${index}.company_name`)} />
+                                <Input label="Title" placeholder="Job titled held..." {...register(`workExperience.${index}.job_title`)} />
+                                <Input label="Location" placeholder="Location..." {...register(`workExperience.${index}.location`)} />
+                                <Input type="date" label="Start Date" placeholder="Start Date..." {...register(`workExperience.${index}.start_date`)} />
+                                <Input type="date" label="End Date" placeholder="End Date..." {...register(`workExperience.${index}.end_date`)} />
 
-                            {/* <Highlights index={index} ref={highlightRef} /> */}
+                                {/* <Highlights index={index} ref={highlightRef} /> */}
+                            </section>
+                            <div className="flex justify-end gap-2">
+                                <Button size="sm" type="button" variant="outline" /**onClick={() => highlightRef.current?.append('')}**/>Add Highlight</Button>
+                                <Button size="sm" className="text-red-400 flex items-center gap-1" type="button" variant="outline" onClick={() => onDeleteClick(field, index)}>
+                                    <Trash2 size={18} />
+                                    <span>Remove Block</span>
+                                </Button>
+                            </div>
                         </section>
-                        <div className="flex justify-end gap-2">
-                            <Button size="sm" type="button" variant="outline" onClick={() => highlightRef.current?.append('')}>Add Highlight</Button>
-                            <Button size="sm" className="text-red-400 flex items-center gap-1" type="button" variant="outline" onClick={() => deleteWorkExperience(field, index)}>
-                                <Trash2 size={18} />
-                                <span>Remove Block</span>
-                            </Button>
-                        </div>
-                    </section>
-                ))}
-                <div className="flex gap-2">
-                    <Button className="text-primary" type="button" variant="outline" onClick={() => append({})}>Add Experience</Button>
-                    <Button loading={isSubmitting}>Proceed</Button>
-                </div>
-            </form>
-        </StepContainer>
+                    ))}
+                    <div className="flex gap-2">
+                        <Button className="text-primary" type="button" variant="outline" onClick={() => append(defaultWorkExperience)}>Add Experience</Button>
+                        <Button loading={isSubmitting}>Proceed</Button>
+                    </div>
+                </form>
+            </StepContainer>
+
+            <AlertDialog
+                open={isOpen}
+                title="Delete Confirmation"
+                description="Are you sure you want to remove this experience"
+                onOk={onDeleteOk}
+                onOpenChange={setIsOpen}
+                onCancel={onCancel}
+                isProcessing={loading}
+            />
+        </>
     )
 }
--- apps/web/src/components/resume-builder/setup/work-experience/highlights.tsx
+++ apps/web/src/components/resume-builder/setup/work-experience/highlights.tsx
@@ -1,43 +1,43 @@
-import { Textarea } from '@components/textarea';
-import { forwardRef, useImperativeHandle } from 'react';
-import { useFieldArray, useFormContext, type UseFieldArrayAppend } from 'react-hook-form';
-import { type FormFields } from 'src/pages/profile/setup';
+// import { Textarea } from '@components/textarea';
+// import { forwardRef, useImperativeHandle } from 'react';
+// import { useFieldArray, useFormContext, type UseFieldArrayAppend } from 'react-hook-form';
+// import { type FormFields } from 'src/pages/profile/setup';
 
-export interface RefTypeWorkExperience {
-    append: UseFieldArrayAppend<FormFields, `workExperience.${number}.highlights`>
-}
+// export interface RefTypeWorkExperience {
+//     append: UseFieldArrayAppend<FormFields, `workExperience.${number}.highlights`>
+// }
 
-export interface HighlightPropTypes {
-    index: number
-}
+// export interface HighlightPropTypes {
+//     index: number
+// }
 
-export const Highlights = forwardRef<RefTypeWorkExperience, HighlightPropTypes>(({ index }, ref) => {
-    const { control } = useFormContext<FormFields>();
-    const { append, fields, remove } = useFieldArray({
-        name: `workExperience.${index}.highlights`,
-        control
-    });
+// export const Highlights = forwardRef<RefTypeWorkExperience, HighlightPropTypes>(({ index }, ref) => {
+//     const { control } = useFormContext<FormFields>();
+//     const { append, fields, remove } = useFieldArray({
+//         name: `workExperience.${index}.highlights`,
+//         control
+//     });
 
-    useImperativeHandle(ref, () => {
-        return {
-            append
-        }
-    }, [append])
+//     useImperativeHandle(ref, () => {
+//         return {
+//             append
+//         }
+//     }, [append])
 
-    return (
-        <section className="col-span-2">
-            {fields.map((field, idx) => {
-                const isFirst = idx === 0
-                // const formProps = register(`workExperience.${index}.highlights.${idx}.text`)
-                return (
-                    <section className="mb-2 flex flex-col items-end" key={field.id}>
-                        <Textarea label={isFirst ? 'Highlights' : ''} containerClasses="col-span-2 col-start-0 w-full mb-1" rows={1} />
-                        <button onClick={() => remove(idx)} className="text-sm hover:underline">Remove</button>
-                    </section>
-                )
-            })}
-        </section>
-    )
-})
+//     return (
+//         <section className="col-span-2">
+//             {fields.map((field, idx) => {
+//                 const isFirst = idx === 0
+//                 // const formProps = register(`workExperience.${index}.highlights.${idx}.text`)
+//                 return (
+//                     <section className="mb-2 flex flex-col items-end" key={field.id}>
+//                         <Textarea label={isFirst ? 'Highlights' : ''} containerClasses="col-span-2 col-start-0 w-full mb-1" rows={1} />
+//                         <button onClick={() => remove(idx)} className="text-sm hover:underline">Remove</button>
+//                     </section>
+//                 )
+//             })}
+//         </section>
+//     )
+// })
 
-Highlights.displayName = 'Highlights'
+// Highlights.displayName = 'Highlights'
