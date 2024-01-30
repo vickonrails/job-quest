f957b5f9c2aa90a03f196ea734ccbe8f9b609b6e
vickonrails <victorofoegbu0009@gmail.com>
2024-01-30 20:49:41 +0100

    refactor: replace Formik with react-hook-form

--- apps/web/src/components/sheet/jobsEditSheet.tsx
+++ apps/web/src/components/sheet/jobsEditSheet.tsx
@@ -1,10 +1,12 @@
+import { ErrorHint } from '@components/resume-builder/setup/error-hint';
 import { useToast } from '@components/toast/use-toast';
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { Formik } from 'formik';
 import { type Database } from 'lib/database.types';
 import { type Job } from 'lib/types';
+import { Controller, useForm } from 'react-hook-form';
 import { Button, Input, Rating, Select, Status_Lookup } from 'ui';
+import { v4 as uuid } from 'uuid';
 import { Sheet, type SheetProps } from './sheet';
 
 interface JobEditSheetProps<T> extends SheetProps {
@@ -20,12 +22,19 @@ export function JobEditSheet<T>(props: JobEditSheetProps<T>) {
     // TODO: abstract this away
     const client = useSupabaseClient<Database>();
     const entity = props.entity as Job;
+    const session = useSession();
     const statusOptions = Status_Lookup.map((x, idx) => ({ value: String(idx), label: x }))
     const { toast } = useToast()
+    const { handleSubmit, reset, register, control, formState: { errors } } = useForm({ defaultValues: entity })
 
     const updateMutation = useMutation({
-        mutationFn: async (data: Job) => {
-            return await client.from('jobs').update(data).eq('id', entity.id)
+        mutationFn: async (data: { job: Job, userId: string }) => {
+            const { job, userId } = data
+            if (!job.id) {
+                job.id = uuid()
+                job.user_id = userId
+            }
+            return await client.from('jobs').upsert(job).eq('id', job.id)
         },
         // TODO: we're currently invalidating the cache now, but we can use setQueryData to just replace the job in the cache
         onSuccess: () => queryClient.invalidateQueries({ queryKey: ['jobs'] })
@@ -33,7 +42,9 @@ export function JobEditSheet<T>(props: JobEditSheetProps<T>) {
 
     const onSubmit = async (job: Job) => {
         try {
-            const { error } = await updateMutation.mutateAsync(job);
+            const userId = session?.user.id
+            if (!userId) return;
+            const { error } = await updateMutation.mutateAsync({ job, userId });
             if (error) {
                 throw error
             }
@@ -54,62 +65,59 @@ export function JobEditSheet<T>(props: JobEditSheetProps<T>) {
     return (
         <Sheet icons={props.icons} {...props}>
             <div className="flex flex-col gap-3">
-                <Formik
-                    initialValues={initialValues}
-                    onSubmit={onSubmit}
-                >
-                    {({ values, handleSubmit, handleChange, setFieldValue, resetForm }) => (
-                        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-                            <Input
-                                autoFocus
-                                placeholder="Job title"
-                                value={values.position}
-                                name="position"
-                                fullWidth
-                                label="Position"
-                                onChange={handleChange}
-                            />
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+                    <Input
+                        autoFocus
+                        placeholder="Job title"
+                        label="Position"
+                        hint={<ErrorHint>{errors.position?.message}</ErrorHint>}
+                        {...register('position', { required: { value: true, message: 'Position is required' } })}
+                    />
 
-                            <div className="mb-4">
-                                <div className="mb-3 text-sm">Rating</div>
+                    <div className="mb-4">
+                        <div className="mb-3 text-sm">Rating</div>
+                        <Controller
+                            name="priority"
+                            control={control}
+                            render={({ field }) => (
                                 <Rating
-                                    onClick={(val) => setFieldValue('priority', val)}
+                                    onClick={(val) => field.onChange(val)}
                                     size="md"
-                                    value={values.priority ?? 0}
+                                    value={field.value ?? 0}
                                 />
-                            </div>
+                            )}
+                        />
 
-                            {/* TODO: figure out why there is no highlight on the select options */}
+                    </div>
+
+                    <Controller
+                        control={control}
+                        name="status"
+                        render={({ field }) => (
                             <Select
-                                name="status"
-                                label="Select"
-                                trigger="Select a status"
-                                // TODO: figure out why just handleChange doesn't seem to work here
-                                onValueChange={(val: string) => setFieldValue('status', Number(val))}
                                 options={statusOptions}
-                                value={String(values.status)}
+                                label="Select"
+                                value={String(field.value)}
+                                trigger="Select a status"
+                                onValueChange={(val: string) => field.onChange(Number(val))}
                             />
-                            <Input
-                                placeholder="Company name"
-                                label="Company name"
-                                name="company_name"
-                                onChange={handleChange}
-                                value={values.company_name}
-                                fullWidth
-                            />
-                            <Input
-                                placeholder="Location"
-                                label="Location"
-                                name="location"
-                                onChange={handleChange}
-                                value={values.location ?? ''}
-                                fullWidth
-                            />
-                            <Button type="submit" loading={updateMutation.isLoading}>Update</Button>
-                            <Button type="button" variant="ghost" onClick={() => resetForm(initialValues)}>Clear Changes</Button>
-                        </form>
-                    )}
-                </Formik>
+                        )}
+                    />
+
+                    <Input
+                        placeholder="Company name"
+                        label="Company name"
+                        hint={<ErrorHint>{errors.company_name?.message}</ErrorHint>}
+                        {...register('company_name', { required: { value: true, message: 'Company name is required' } })}
+                    />
+                    <Input
+                        placeholder="Location"
+                        label="Location"
+                        {...register('location')}
+                    />
+                    <Button type="submit" loading={updateMutation.isLoading}>Update</Button>
+                    <Button type="button" variant="ghost" onClick={() => reset(initialValues)}>Clear Changes</Button>
+                </form>
             </div>
         </Sheet>
     )
