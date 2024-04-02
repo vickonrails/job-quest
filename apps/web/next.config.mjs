c3334303f756985d23bb5a5cce21f07130f13f0a
vickonrails <victorofoegbu0009@gmail.com>
2024-04-02 16:53:07 +0100

    refactor: remove build step in shared

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,11 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'));
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  transpilePackages: ['shared'],
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
@@ -15,8 +16,8 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   images: {
     domains: [
