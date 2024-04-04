15d54faff94f0c114e9ba61b681f0a395bc84bac
vickonrails <victorofoegbu0009@gmail.com>
2024-04-04 11:13:14 +0100

    refactor: order jobs by desc, fix extension order_column when adding jobs

--- apps/extension/types.ts
+++ apps/extension/types.ts
@@ -1,4 +1,4 @@
-import { type Database } from 'shared/dist/types'
+import { type Database } from 'shared'
 
 export type Job = Database['public']['Tables']['jobs']['Row'] & { img?: string }
 export type JobInsertDTO = Database['public']['Tables']['jobs']['Insert'] & { img?: string, notes?: string }
