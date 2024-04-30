b391f333f61049dd94e9422cca792c7ce57b980c
vickonrails <victorofoegbu0009@gmail.com>
2024-04-30 21:08:05 +0100

    feat: browser extension logout

--- apps/extension/utils/use-auth.ts
+++ apps/extension/utils/use-auth.ts
@@ -2,13 +2,26 @@ import { sendToBackground } from '@plasmohq/messaging';
 import { useStorage } from '@plasmohq/storage/hook';
 import type { Session } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
+import { supabase } from '~core/supabase';
 
 /**
  * useAuth hook to return current user session
  */
 export function useAuth() {
-    const [session] = useStorage<Session>('session')
+    const [session, setSession] = useStorage<Session>('session')
     const [firstRender, setFirstRender] = useState(true)
+
+    useEffect(() => {
+        supabase.auth.onAuthStateChange((ev, session) => {
+            if (!session && ev === 'SIGNED_OUT') {
+                setSession(null).then(res => {
+                    // 
+                }).catch(err => {
+                    // 
+                })
+            }
+        })
+    }, [setSession])
 
     // This useEffect is a hack to solve the bug in useStorage that makes the first render value undefined.
     useEffect(() => {
